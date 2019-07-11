0a506cdb7702fa27e1084543414a369d8d43ed59
Eloy dos Santos Barbosa da Silva <eloy.silva@netmaster.intra>
2019-07-11 16:23:22 -0300

    subindo projeto

---
+++ features/specs/tentativa_login.feature
@@ -0,0 +1,8 @@
+#language: pt
+
+@tentativa_login
+Funcionalidade: Efetuar Login no Site Centauro.
+
+Cenario: Efetuando login no site centauro.
+Quando eu tento efetuar um login no site centauro.
+Então verifico se estou logado.
---
+++ features/step_definitions/tentativa_login.rb
@@ -0,0 +1,14 @@
+Quando("eu tento efetuar um login no site centauro.") do
+  visit '/slogin'
+
+  fill_in(id: 'input-type-1', with: 'eloy.testesbnet@gmail.com')
+  find('input[id="optYesclient"]').click
+  fill_in(id: 'input-type-4', with: '123456')
+  find('input[id="btn_Entrar"]').click
+end
+
+Então("verifico se estou logado.") do
+  validar_login = find(class: "user-tag")
+  expect(validar_login.text).to eq 'Olá, Eloy'
+  sleep(5)
+end
